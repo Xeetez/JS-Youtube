e14b4a63c9b35c3cea5a27f31c8bf3563a44697c
Xeetez <pdlkshitiz@gmail.com>
2024-01-02 20:20:22 +1100

    prefix and postfix

--- basic01/conversionOperation.js
+++ basic01/conversionOperation.js
@@ -22,5 +22,40 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/* OPERATIONS */
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Kshitiz"
+let str2 = " Paudel"
+let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((1 + 2) + 5 % 3);
+
+// console.log(+ "");
+
+let num1, num2, num3
+
+num1= num2 = num3 = 2+2
+
+// console.log(num1);
+
+let gameCounter = 100
+gameCounter++
+// console.log(gameCounter);
+
+let x = 3
+let y = x++ //increment afterwards, x =4 and y => 3
+
+console.log(`x:${x} y: ${y}`);
+let a = 4
+let b = ++a // increments before and set a as +1
+console.log(`a:${a} y: ${b}`);
+
+
