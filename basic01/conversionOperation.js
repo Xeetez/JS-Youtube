4b2f6dee4cfe871375ccf851f5d1d33d5d4bdd33
Xeetez <pdlkshitiz@gmail.com>
2024-01-02 19:58:20 +1100

    conversion of datatypes

---
+++ basic01/conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "hitesh"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNum = Number(score); //changing string to int
+// value will be NaN
+// console.log(valueInNum);
+// console.log(typeof valueInNum);
+// "33" => 33
+// "33abs" => NaN (Not a number)
+// true => 1 ; flase => 0
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+//booleans conversion
+// 1 => boolean 0=> false
+// "" => false "afsd" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
