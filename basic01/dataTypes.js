afa1c7aba640c7d1ca1245caacdd5030d12002ac
Xeetez <pdlkshitiz@gmail.com>
2024-01-02 19:41:28 +1100

    data types

---
+++ basic01/dataTypes.js
@@ -0,0 +1,12 @@
+let name = "Kshitiz Paudel";
+let age = 30;
+let address = "2/4 walter street South Launceston 7248";
+let info;
+let sex = null;
+
+console.table([name, age, address, info, sex]);
+
+console.log(typeof sex); // null is an object
+console.log(typeof info);// undefined is a varibale i.e. data type
+
+// Do not use var as it is a old version and has limited scope
